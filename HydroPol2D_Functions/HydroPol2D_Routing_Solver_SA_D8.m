9b294db206a897cd831ea61e505cfed58c41f0f4
Marcus N. Gomes Jr <85426912+marcusnobrega-eng@users.noreply.github.com>
2024-06-14 13:26:52 -0700

    Update HydroPol2D_Routing_Solver_SA_D8.m

--- HydroPol2D_Functions/HydroPol2D_Routing_Solver_SA_D8.m
+++ HydroPol2D_Functions/HydroPol2D_Routing_Solver_SA_D8.m
@@ -1,6 +1,6 @@
 % HydroPol2D Solver
 
-function [Qmod, Cmod,Dmod,Flooded_Area,Risk_Area] = HydroPol2D_Routing_Solver_SA_D8(wse_slope_zeros,Distance_Matrix,min_soil_moisture,BC_States, CA_States, Courant_Parameters, date_begin, DEM_raster, depths, Elevation_Properties, flags, gauges, GIS_data, Human_Instability, Hydro_States, idx_nan, idx_nan_5, idx_outlet, Inflow_Parameters, LULC_Properties, Maps, nx_max, ny_max, outlet_index, outlet_runoff_volume, outlet_type, Rainfall_Parameters, recording_parameters, running_control, slope_outlet, Soil_Properties, t_previous, time_calculation_routing, time_step, time_step_model, tmin_wq, WQ_States, Wshed_Properties,idx_rivers,Lateral_Groundwater_Flux,Reservoir_Data,Input_Rainfall)
+function [Qmod, Cmod,Dmod,Flooded_Area,Risk_Area] = HydroPol2D_Routing_Solver_SA_D8(wse_slope_zeros,Distance_Matrix,min_soil_moisture,BC_States, CA_States, Courant_Parameters, date_begin, DEM_raster, depths, Elevation_Properties, flags, gauges, GIS_data, Human_Instability, Hydro_States, idx_nan, idx_nan_5, idx_outlet, Inflow_Parameters, LULC_Properties, Maps, nx, ny_max, outlet_index, outlet_runoff_volume, outlet_type, Rainfall_Parameters, recording_parameters, running_control, slope_outlet, Soil_Properties, t_previous, time_calculation_routing, time_step, time_step_model, tmin_wq, WQ_States, Wshed_Properties,idx_rivers,Lateral_Groundwater_Flux,Reservoir_Data,Input_Rainfall,outflow_bates)
 
 
 z2_save = 0;
