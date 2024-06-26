70c3a461ca961205aa9ca9311f7cba6da2e22085
Marcus N. Gomes Jr <85426912+marcusnobrega-eng@users.noreply.github.com>
2024-06-25 17:16:56 -0700

    Update Bates_Inertial_8D.m
    Added critical velocity limitation in the code
    
    Added wet/dry bed treatment

--- HydroPol2D_Functions/Bates_Inertial_8D.m
+++ HydroPol2D_Functions/Bates_Inertial_8D.m
@@ -1,5 +1,5 @@
 %@ -0,0 +1,242 @@
-function [qout_left,qout_right,qout_up,qout_down,outlet_flow,qout_ne,qout_se,qout_sw,qout_nw,d_t,I_tot_end_cell,outflow,Hf] = Bates_Inertial_8Dreservoir_x,reservoir_y,k1,h1,k2,k3,h2,k4,yds1,xds1,yds2,xds2,flag_reservoir,z,d_tot,d_p,roughness_cell,cell_area,time_step,Resolution,outlet_index,outlet_type,slope_outlet,row_outlet,col_outlet,d_tolerance,outflow,idx_nan)
+function [qout_left,qout_right,qout_up,qout_down,outlet_flow,qout_ne,qout_se,qout_sw,qout_nw,d_t,I_tot_end_cell,outflow,Hf] = Bates_Inertial_8Dreservoir_x,reservoir_y,k1,h1,k2,k3,h2,k4,yds1,xds1,yds2,xds2,flag_reservoir,z,d_tot,d_p,roughness_cell,cell_area,time_step,Resolution,outlet_index,outlet_type,slope_outlet,row_outlet,col_outlet,d_tolerance,outflow,idx_nan,flag_critical)
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 %                                                                 %
 %                 Produced by Marcus Nobrega Gomes Junior         %
@@ -136,8 +136,82 @@ g = 9.81;
 outflow_prev = outflow;
 outflow = (outflow_prev - g*Hf*dt.*matrix_store)./ ...
           (1 + g*Hf*dt.*roughness_cell.^2.*outflow_prev./(Hf.^(10/3))); % m2 per sec (Hf can be simplified)
-outflow = Resolution*outflow/(Resolution^2)*1000*3600; % mm per hour
+
+%% Eliminating Surplus Velocities at Wet-Dry Interfaces
+% --------------- All NE, SE, SW, NW  % ---------------%
+% 6, 7, 8, 9, respectively
+% Left 
+idx = Hf(:,:,1) > artificial_depth & [Hf(:,1:(end-1),1),zeros(size(depth_cell,1),1)] == artificial_depth;
+interface_flow = outflow(:,:,1);
+depth = Hf(:,:,1);
+if any(any(idx)) 
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx)); % m2 per sec
+    outflow(:,:,1) = interface_flow;   
+end
+% Right
+idx = Hf(:,:,2) > artificial_depth & [zeros(size(depth_cell,1),1), Hf(:,2:(end),2)] == artificial_depth;
+interface_flow = outflow(:,:,2);
+depth = Hf(:,:,2);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,2) = interface_flow;
+end
+% Up
+idx = Hf(:,:,3) > artificial_depth & [zeros(1,size(depth_cell,2));Hf(1:(end-1),:,3)] == artificial_depth;
+interface_flow = outflow(:,:,3);
+depth = Hf(:,:,3);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,3) = interface_flow;
+end
+% Down
+idx = Hf(:,:,4) > artificial_depth & [Hf(2:(end),:,4);zeros(1,size(depth_cell,2))] == artificial_depth;
+interface_flow = outflow(:,:,4);
+depth = Hf(:,:,4);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,4) = interface_flow;
+end
+% NE
+idx = Hf(:,:,6) > artificial_depth & [zeros(1,size(depth_cell,2));Hf(1:(end-1),(2:end),6)] == artificial_depth;
+interface_flow = outflow(:,:,4);
+depth = Hf(:,:,6);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,6) = interface_flow;
+end
+% SE
+idx = Hf(:,:,7) > artificial_depth & [Hf(2:(end),(2:end),7),zeros(1,size(depth_cell,2))] == artificial_depth;
+interface_flow = outflow(:,:,4);
+depth = Hf(:,:,7);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,7) = interface_flow;
+end
+% SW
+idx = Hf(:,:,8) > artificial_depth & [Hf(2:(end),(1:end-1),8),zeros(1,size(depth_cell,2))] == artificial_depth;
+interface_flow = outflow(:,:,8);
+depth = Hf(:,:,8);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,8) = interface_flow;
+end
+% NW
+idx = Hf(:,:,9) > artificial_depth & [zeros(1,size(depth_cell,2));Hf(1:(end-1),(1:end-1),0)] == artificial_depth;
+interface_flow = outflow(:,:,9);
+depth = Hf(:,:,9);
+if any(any(idx))
+    interface_flow(idx) = depth(idx).*sqrt(g*depth(idx));
+    outflow(:,:,9) = interface_flow;
+end
+%% Limiting outflow to critical velocity
+if flag_critical == 1
+    critical_velocity = Hf.*sqrt(g*Hf);
+    outflow = min(outflow,critical_velocity);
+end
+
 % Taking out negative values
+outflow = Resolution*outflow/(Resolution^2)*1000*3600; % mm per hour
 outflow(outflow<0) = 0;
 outflow(isnan(outflow)) = 0; outflow(isinf(outflow)) = 0;
 outflow(mask) = 0;
