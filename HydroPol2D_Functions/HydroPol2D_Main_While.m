abb145ee6b5e81acc791f0b392336784d918ee6b
Marcus N. Gomes Jr <85426912+marcusnobrega-eng@users.noreply.github.com>
2024-07-03 12:20:55 -0700

    Update HydroPol2D_Main_While.m
    Just fixed the case where no infiltration is occuring (mass balance check)

--- HydroPol2D_Functions/HydroPol2D_Main_While.m
+++ HydroPol2D_Functions/HydroPol2D_Main_While.m
@@ -9,7 +9,7 @@
 % clear all
 % load workspace_franquinho.mat
 % save('preprocessing_input.mat');
-format long
+format short
 
 tic 
 k = 1; % time-step counter
@@ -267,20 +267,16 @@ while t <= (running_control.routing_time + running_control.min_time_step/60) % R
     % Storage Calculation
     previous_storage = current_storage;
     current_storage = nansum(nansum(Wshed_Properties.cell_area*depths.d_t/1000)); % m3
+    if flags.flag_infiltration == 0
+        Hydro_States.f = zeros(size(outlet_states.outlet_flow,1),size(outlet_states.outlet_flow,2));
+    end    
     % dS/dt = Qin - Qout = Rain + Inflow - Outflow - infiltration
     delta_storage = current_storage - previous_storage;
     outflow_flux = nansum(nansum(outlet_states.outlet_flow))/1000*Wshed_Properties.cell_area/3600 + ...
                     nansum(nansum(Hydro_States.f/1000/3600*Wshed_Properties.cell_area)); % m3 per sec
     flux_volumes = inflow_vol - outflow_flux*time_step*60; % dt(Qin - Qout) m3
     volume_error(k,1) = flux_volumes - delta_storage;    
-
-    % Introducing the volume error to the inflow cells
-    if flags.flag_inflow == 1
-        mask = logical(Wshed_Properties.inflow_cells);
-        if any(any(mask))
-            depths.d_t(mask) = depths.d_t(mask) + 1000*max(volume_error(k,1))/sum(sum(mask))/Wshed_Properties.cell_area;
-        end
-    end
+    
     % Refreshing time-step script
     refreshing_timestep;
     
