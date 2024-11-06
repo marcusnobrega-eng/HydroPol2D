b77f671b7e18890177323edba3ac2b2933185b69
luiscastillo1993 <106620265+luiscastillo1993@users.noreply.github.com>
2024-11-06 20:25:47 -0300

    Delete HydroPol2D_Functions/HydroPol2D_Functions directory

--- HydroPol2D_Functions/HydroPol2D_Functions/gaussian_noise_generator.m
+++
@@ -1,7 +0,0 @@
-%%% Gaussian Noise %%%
-% Goal - Create a single number representing a gaussian noise
-function [g_noise] = gaussian_noise_generator(variance,average)
-std_deviation = sqrt(variance);
-% g_noise = (average + std_deviation*randn)
-g_noise = average + std_deviation*randn;
-end
